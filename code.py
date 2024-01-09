637efd33600bd506dcf30d1d5fd6ecf1173fea8b
PANANABIK666K <130806262+PANANABIK666K@users.noreply.github.com>
2024-01-09 22:10:58 +1000

    Update code.py

--- code.py
+++ code.py
@@ -7,32 +7,15 @@ import io
 
 app = Dash(__name__)
 
+data = pd.read_csv('data.csv')
+
 app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        multiple=False  # Allow only single file upload
-    ),
-    html.Div(id='output-data-upload'),
     html.Div(children='Погода'),
     html.Hr(),
     dcc.RadioItems(options=[
-        {'label': 'ветер день', 'value': 'ветер день'},
-        {'label': 'Температура день', 'value': 'Температура день'}
-    ], value='ветер день', id='controls-and-radio-item'),
+        {'label': 'давление вечер', 'value': 'давление вечер'},
+        {'label': 'облачность день', 'value': 'облачность день'}
+    ], value='давление вечер', id='controls-and-radio-item'),
     dash_table.DataTable(data=[], page_size=10, id='data-table'),
     dcc.Graph(figure={}, id='controls-and-graph'),
     dcc.Graph(figure={}, id='histogram-chart'),
@@ -44,23 +27,28 @@ app.layout = html.Div([
      Output(component_id='controls-and-graph', component_property='figure'),
      Output(component_id='histogram-chart', component_property='figure'),
      Output(component_id='line-chart', component_property='figure')],
-    [Input(component_id='upload-data', component_property='contents'),
-     Input(component_id='upload-data', component_property='filename'),
-     Input(component_id='controls-and-radio-item', component_property='value')]
+    [
+     Input(component_id='controls-and-radio-item', component_property='value')
+    ]
 )
-def update_data_and_graph(contents, filename, col_chosen):
+def update_data_and_graph(contents):
+    if contents is None:
+        contents = 'давление вечер'
     if contents is not None:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
 
-        fig_hist = px.histogram(df, x='id города', y=col_chosen, histfunc='avg')
-        fig_pie = px.pie(df, values='id города', names='облачность день')
-        fig_bar = px.bar(df, x='ветер день', y='id города')
+        count = len(data['ветер день'])
+        for i in range(count):
+            d = str(data['ветер день'][i]).split(' ')[0]
+            data['ветер день'][i] = d
 
-        return df.to_dict('records'), fig_pie, fig_hist, fig_bar
+        fig_hist = px.histogram(data, x=data['id города'], y=data[contents])
+        fig_pie = px.pie(data, values=data['id города'], names=data['ветер день'])
+        fig_bar = px.bar(data, x=data['давление день'], y=data['id города'])
+
+        return data.to_dict('records'), fig_pie, fig_hist, fig_bar
     else:
         return [], {}, {}, {}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
